4101f2e4109603b87f0327e2e7dc2a95e2c80738
zhoujunlin <2450440507@qq.com>
2026-09-07 09:40:13 +0800

    1.调用大模型

---
+++ char_with_model_copy/1_调用大模型.py
@@ -0,0 +1,23 @@
+import os
+from openai import OpenAI
+
+#1.创建【模型】客户端对象
+client = OpenAI(
+    api_key="sk-02aa824e3e2442dd97cbf80f8280ae96",
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
+
+#2.调用模型 5c
+completion = client.chat.completions.create(
+    model="qwen3.8-max",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "你是谁？"},
+    ]
+)
+
+#3.打印输出
+print(completion.model_dump_json())
+
+#3.1打印指定输出内容
+print(completion.choices[0].message.content)
